721de246bd2be225840a423da49f9c2f57b72e82
Asfii <asfi528@gmail.com>
2021-11-15 10:44:00 +0500

    first commit

---
+++ app/src/main/java/com/example/secondappivy/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.secondappivy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+
+
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue requestQueue;
+    private TextView textView;
+    private static final String URL ="http://192.168.100.125:3000/api/zone";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textview_hello_world);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+
+
+        JsonArrayRequest stringRequest = new JsonArrayRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i=0;i<response.length();i++){
+                            try {
+                                JSONObject jsonObject= response.getJSONObject(i);
+                                Log.d("API",jsonObject.getString("uid"));
+                                textView.setText(jsonObject.getString("uid"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("invalid response");
+            }
+        });
+
+        requestQueue.add(stringRequest);
+
+    }
+}
+
---
+++ app/src/main/java/com/example/secondappivy/model/Zone.java
@@ -0,0 +1,24 @@
+package com.example.secondappivy.model;
+
+public class Zone {
+    int uid;
+    String name;
+    String publish_type;
+    String timestamp;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPublish_type() {
+        return publish_type;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+}
